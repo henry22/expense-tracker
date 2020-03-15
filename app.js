d3c931196a90b9761886f8f0e6d5b012f5e20e28
henry22 <henry811010@gmail.com>
2020-03-15 18:36:05 +0800

    Install method-override and use it

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const db = mongoose.connection
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const handlebars = require('handlebars')
+const methodOverride = require('method-override')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 handlebars.registerHelper('isEqual', (arg1, arg2, options) => {
   return arg1 === arg2 ? options.fn(this) : options.inverse(this)
