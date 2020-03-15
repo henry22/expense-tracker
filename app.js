bc6f79f79089ae7f5deae3fed7ca68bc52bfed4e
henry22 <henry811010@gmail.com>
2020-03-15 14:49:03 +0800

    Install mongoose and set mongoose connection and add record model schema in models

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 3000
+const mongoose = require('mongoose')
+const db = mongoose.connection
+
+mongoose.connect('mongodb://localhost/expense-tracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+db.on('error', () => console.log('mongodb error'))
+db.once('open', () => console.log('mongodb connect'))
 
 app.get('/', (req, res) => {
   res.send('expense tracker')
 })
-
 
 app.listen(port, () => console.log('Server is running'))
