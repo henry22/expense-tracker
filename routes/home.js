5a64f35b4d3daeb0b2a9ea20f4b6f046779a02cb
henry22 <henry811010@gmail.com>
2020-03-17 18:23:54 +0800

    Pass the month to show on month dropdown list

--- routes/home.js
+++ routes/home.js
@@ -9,14 +9,36 @@ const {authenticated} = require('../config/auth')
 // Index 頁面/首頁
 router.get('/', authenticated, (req, res) => {
   let dropdownText = '類別'
+  let dropdownMonthText = '月份'
   const category = req.query.category
+  const month = req.query.month
   const selectCategory = {}
+  const months = {
+    '01': '一月',
+    '02': '二月',
+    '03': '三月',
+    '04': '四月',
+    '05': '五月',
+    '06': '六月',
+    '07': '七月',
+    '08': '八月',
+    '09': '九月',
+    '10': '十月',
+    '11': '十一月',
+    '12': '十二月'
+  }
 
   if (category) {
     selectCategory.category = category
     dropdownText = category
   } else {
     dropdownText = '全部'
+  }
+
+  if(month) {
+    dropdownMonthText = months[month]
+  } else {
+    dropdownMonthText = '全部'
   }
 
   Record.find({userId: req.user._id})
@@ -30,7 +52,8 @@ router.get('/', authenticated, (req, res) => {
         record.date = moment(record.date).format('YYYY/MM/DD')
         record.category = replaceIcon(record.category)
       })
-      res.render('index', {records: records, dropdownText: dropdownText, totalAmount: totalAmount})
+
+      res.render('index', { records: records, dropdownText: dropdownText, dropdownMonthText: dropdownMonthText, totalAmount: totalAmount})
     })
 })
 
